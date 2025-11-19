abf66cc995e6773cc1ba508c5a097443f137288a
STES沐霖韵 <3549337307@qq.com>
2025-11-20 01:31:17 +0800

    Add files via upload

--- run.bat
+++ run.bat
@@ -1,4 +1,9 @@
 @echo off
 chcp 65001 >nul
 cd /d "%~dp0"
+title LinBot 启动器
+
+echo ================================
+echo        LinBot 启动器
+echo ================================
 powershell -ExecutionPolicy Bypass -Command "& './venv/Scripts/Activate.ps1'; cd 'linbot'; python 'run_with_reload.py'; pause"
