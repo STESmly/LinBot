d997cff70e0183494fefb91a86c951685a8e440a
STES沐霖韵 <3549337307@qq.com>
2025-11-26 00:18:39 +0800

    up

--- run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-chcp 65001 >nul
-cd /d "%~dp0"
-title LinBot 启动器
-
-echo ================================
-echo        LinBot 启动器
-echo ================================
-powershell -ExecutionPolicy Bypass -Command "& './venv/Scripts/Activate.ps1'; cd 'linbot'; python 'run_with_reload.py'; pause"
